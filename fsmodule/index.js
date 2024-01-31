16d22b237afd951f9884444ac4b6f57d11d5fd3a
Ayazkhan <ayaz.khan1990@gmail.com>
2024-01-31 19:36:23 +0530

    fs modules

--- fsmodule/index.js
+++ fsmodule/index.js
@@ -1,6 +1,32 @@
 const fs = require('fs');
-//non bloking IO Modle
-fs.writeFile("file.txt", "It is Sample Data", ()=>{
-console.log("Data is Successfully added");
-})
-console.log("Another data");
+// //============non bloking IO Modle
+// fs.writeFile("file.txt", "Data writen by Ayaz khan", ()=>{
+// console.log("Data is Successfully added");
+// })
+// console.log("Another data");
+
+
+//============bloking IO Modle Write file with Sync
+// const b= fs.writeFileSync("file1.txt", "Another data",)
+//     console.log(b);
+    
+//     console.log("Another data");
+
+//     //=========Read File
+// fs.readFile("file.txt", "utf-8", (err, data)=>{
+//     console.log(data);
+// })
+
+    // //=========Read File
+    // fs.appendFile("file.txt", " Appand file ka data add", (err)=>{
+    //     console.log("Succesfull");
+    // })
+
+    //  //========= File Name Rename 
+    //  fs.rename("file.txt", "changefile.txt", (err)=>{
+    //     console.log("Succesfull");
+    // })
+
+    //========= File delete
+    fs.unlinkSync("file1.txt");
+    
