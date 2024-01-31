a64efc94d1651c94f2509418c7ad36bafde7fa60
Ayazkhan <ayaz.khan1990@gmail.com>
2024-01-31 20:03:28 +0530

    os module

---
+++ osmodules/index.js
@@ -0,0 +1,10 @@
+// Memory dekhne k liy ki kitna space hai
+
+const os= require('os');
+
+console.log(os.freemem());
+
+// check home directory
+console.log(os.homedir());
+console.log(os.machine());
+console.log(os.networkInterfaces());
