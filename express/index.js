56d9d3bdaf455056819a54c22ea35e9724980bd2
Ayazkhan <ayaz.khan1990@gmail.com>
2024-02-01 21:59:43 +0530

    download file add

--- express/index.js
+++ express/index.js
@@ -1,11 +1,24 @@
 const express= require("express");
+const path=require("path")
 const app=express()
 
+const pathdir=path.join(__dirname,'public')
+// app.use(express.static(pathdir));
+
 app.get('/', (req, res)=>{
-    res.send("Hi This Express");
+    res.sendFile(`${pathdir}/index.html`);
+})
+app.get('/home', (req, res)=>{
+    res.sendFile(`${pathdir}/home.html`);
+})
+app.get('/download', (req, res)=>{
+    res.download(`${pathdir}/home.html`);
 })
 app.get('/about', (req, res)=>{
-    res.send("Hi This About root");
+    res.send({
+    name: "Ayaz",
+    Age: 30
+    });
 })
 
 app.listen(3000, ()=>{
