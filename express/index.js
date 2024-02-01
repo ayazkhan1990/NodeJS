83977c0ba61ca707929f27c83aeb5a0370abd594
Ayazkhan <ayaz.khan1990@gmail.com>
2024-02-01 23:19:41 +0530

    ejs template

--- express/index.js
+++ express/index.js
@@ -2,23 +2,34 @@ const express= require("express");
 const path=require("path")
 const app=express()
 
-const pathdir=path.join(__dirname,'public')
+// const pathdir=path.join(__dirname,'public')  
 // app.use(express.static(pathdir));
 
-app.get('/', (req, res)=>{
-    res.sendFile(`${pathdir}/index.html`);
+// app.get('/', (req, res)=>{
+//     // res.sendFile(`${pathdir}/index.html`);  //direct html file
+// })
+// app.get('/home', (req, res)=>{
+//     // res.sendFile(`${pathdir}/home.html`);   //direct html file
+// })
+// app.get('/download', (req, res)=>{
+//     // res.download(`${pathdir}/home.html`);  //direct html file
+// })
+// app.get('/about', (req, res)=>{
+//     res.send({
+//     name: "Ayaz",
+//     Age: 30
+//     });
+// })
+
+// =================== 2nd way
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+    res.render("index", {title:"index"})
 })
-app.get('/home', (req, res)=>{
-    res.sendFile(`${pathdir}/home.html`);
-})
-app.get('/download', (req, res)=>{
-    res.download(`${pathdir}/home.html`);
-})
-app.get('/about', (req, res)=>{
-    res.send({
-    name: "Ayaz",
-    Age: 30
-    });
+app.get("/home", (req, res)=>{
+    res.render("home", {title:"home"})
 })
 
 app.listen(3000, ()=>{
